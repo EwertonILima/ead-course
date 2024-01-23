00014b2a086a37be2acc8290581e1f1f3578be2b
ewertonilima <ewertoninaciolima@gmail.com>
2024-01-23 08:27:21 -0300

    :bug: fix: Resolve issue with incorrect id name

--- src/main/java/com/ewertonilima/course/repositories/LessonRepository.java
+++ src/main/java/com/ewertonilima/course/repositories/LessonRepository.java
@@ -13,5 +13,5 @@ import java.util.UUID;
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 
     @Query(value = "select * from tb_lessons where module_module_id = :moduleId", nativeQuery = true)
-    List<LessonModel> findAllLessonsIntoModule(@Param("mooduleId") UUID moduleId);
+    List<LessonModel> findAllLessonsIntoModule(@Param("moduleId") UUID moduleId);
 }
