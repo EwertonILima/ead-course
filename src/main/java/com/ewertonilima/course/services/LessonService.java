a302f36a122c9b2dae6356300d75928857ee0d06
ewertonilima <ewertoninaciolima@gmail.com>
2024-01-25 10:11:41 -0300

    :sparkles: feat: Add CRUD for lesson controller, lesson dto with validation, customized queries

--- src/main/java/com/ewertonilima/course/services/LessonService.java
+++ src/main/java/com/ewertonilima/course/services/LessonService.java
@@ -1,4 +1,17 @@
 package com.ewertonilima.course.services;
 
+import com.ewertonilima.course.models.LessonModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LessonService {
+    LessonModel save(LessonModel lessonModel);
+
+    void delete(LessonModel lessonModel);
+
+    Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId);
+
+    List<LessonModel> findAllByModule(UUID moduleId);
 }
--- src/main/java/com/ewertonilima/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ewertonilima/course/services/impl/LessonServiceImpl.java
@@ -1,13 +1,38 @@
 package com.ewertonilima.course.services.impl;
 
+import com.ewertonilima.course.models.LessonModel;
 import com.ewertonilima.course.repositories.LessonRepository;
 import com.ewertonilima.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class LessonServiceImpl implements LessonService {
 
     @Autowired
     LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModule(moduleId, lessonId);
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);
+    }
 }
