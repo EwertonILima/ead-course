688eb99292d9d753e43ee729249ea70339042325
ewertonilima <ewertoninaciolima@gmail.com>
2024-02-02 19:40:14 -0300

    🚧 WIP: Prepare GetAll method to API Composition Pattern
    New advanced filter with Specification

--- src/main/java/com/ewertonilima/course/controllers/CourseController.java
+++ src/main/java/com/ewertonilima/course/controllers/CourseController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -85,8 +86,14 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<Page<CourseModel>> getALlCourses(SpecificationTemplate.CourseSpec spec,
                                                            @PageableDefault(page = 0, size = 10, sort = "courseId",
-                                                                   direction = Sort.Direction.ASC) Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
+                                                                   direction = Sort.Direction.ASC) Pageable pageable,
+                                                           @RequestParam(required = false) UUID userId) {
+        if (userId != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(SpecificationTemplate
+                    .courseUserId(userId).and(spec), pageable));
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
+        }
     }
 
     @GetMapping("/{courseId}")
--- src/main/java/com/ewertonilima/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ewertonilima/course/specifications/SpecificationTemplate.java
@@ -1,6 +1,7 @@
 package com.ewertonilima.course.specifications;
 
 import com.ewertonilima.course.models.CourseModel;
+import com.ewertonilima.course.models.CourseUserModel;
 import com.ewertonilima.course.models.LessonModel;
 import com.ewertonilima.course.models.ModuleModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
@@ -10,6 +11,7 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.Collection;
 import java.util.UUID;
@@ -50,6 +52,14 @@ public class SpecificationTemplate {
             Expression<Collection<LessonModel>> moduleLessons = module.get("lessons");
             return criteriaBuilder.and(criteriaBuilder.equal(module.get("moduleId"), moduleId),
                     criteriaBuilder.isMember(lesson, moduleLessons));
+        };
+    }
+
+    public static Specification<CourseModel> courseUserId(final UUID userId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Join<CourseModel, CourseUserModel> courseProd = root.join("coursesUsers");
+            return cb.equal(courseProd.get("userId"), userId);
         };
     }
 }
