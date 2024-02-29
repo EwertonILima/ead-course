2e592ac52821ebcac1ada45ff11580312edf90d5
ewertonilima <ewertoninaciolima@gmail.com>
2024-02-29 07:35:31 -0300

    🔧 Implements RabbitMQ configuration.
    ➕ Add Spring RabbitMQ AMQP dependency

---
+++ src/main/java/com/ewertonilima/course/configs/RabbitmqConfig.java
@@ -0,0 +1,33 @@
+package com.ewertonilima.course.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    final CachingConnectionFactory cachingConnectionFactory;
+
+    public RabbitmqConfig(CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate template() {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
